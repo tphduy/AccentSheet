4caa770b9f4ed2dc82febb802e7f65c2429ed4cf
Duy Tran <tphduy@gmail.com>
2023-05-25 16:06:27 +0700

    Initial Commit

---
+++ SwiftUI Playground/SwiftUI_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PlaygroundApp.swift
+//  SwiftUI Playground
+//
+//  Created by Duy Tran on 25/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
